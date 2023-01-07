9d12162777e449c3a2251d0653f909a63a12091b
GnaixEuy <sgghcj@live.com>
2023-01-07 20:30:10 +0800

    :beer: 全局异常处理和统一返回格式

---
+++ common/service_base/src/main/java/cn/GnaixEuy/servicebase/config/exception/BizException.java
@@ -0,0 +1,25 @@
+package cn.GnaixEuy.servicebase.config.exception;
+
+/**
+ * <img src="http://blog.gnaixeuy.cn/wp-content/uploads/2022/09/倒闭.png"/>
+ *
+ * <p>项目： gxschool_parent </p>
+ * 创建日期： 2023/1/7
+ *
+ * @author GnaixEuy
+ * @version 1.0.0
+ * @see <a href="https://github.com/GnaixEuy"> GnaixEuy的GitHub </a>
+ */
+public class BizException extends RuntimeException {
+
+    private final Integer code;
+
+    public BizException(ExceptionType exceptionType) {
+        super(exceptionType.getMessage());
+        this.code = exceptionType.getCode();
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+}
---
+++ common/service_base/src/main/java/cn/GnaixEuy/servicebase/config/exception/ErrorResponse.java
@@ -0,0 +1,26 @@
+package cn.GnaixEuy.servicebase.config.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * <img src="http://blog.gnaixeuy.cn/wp-content/uploads/2022/09/倒闭.png"/>
+ *
+ * <p>项目： gxschool_parent </p>
+ * 创建日期： 2023/1/7
+ *
+ * @author GnaixEuy
+ * @version 1.0.0
+ * @see <a href="https://github.com/GnaixEuy"> GnaixEuy的GitHub </a>
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private Integer code;
+
+    private String message;
+
+    private Object trace;
+}
